3a6cb92db704a54b9fcf7be76116e461a7c65e10
DheerajGaur.Official <dheeraj.ajaygaur05@gmail.com>
2026-07-18 17:49:25 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ global.wss = wss;
 app.use(cors({
   origin: [
     'http://localhost:5173',
-    'http://localhost:3000',
+    'http://localhost:5000',
     'https://mahirandfriends.vercel.app',
     'https://mahirandfriends.onrender.com'
   ],
